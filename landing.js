02e8eadd6eaf4fccdd85c2f345fc576250de066e
Dharaneeshwar <daranip@gmail.com>
2020-07-30 21:21:08 +0530

    VERSION 1.0

--- landing.js
+++ landing.js
@@ -16,14 +16,37 @@ type = "all";
 // ACHIEVE DATA ENTRY -------------------------------------------------------
 achieve_data = [
   {
-    title: "Introduction to Python",
-    content: "This is a Course completion certificate from suma",
-    img_link: "/tiny_image/intro_python.jpg",
-    document: "documents/resume.pdf",
-    type: "complete",
+    title: "Programming with python for Data Science",
+    content: "Completed a course on NIIT with Outstanding performance.",
+    img_link: "/tiny_image/Python for data science.jpg",
+    document: "https://drive.google.com/file/d/1IsWIyVFDbxq02xP0Of20L5zPqS9818vJ/view?usp=sharing",
+  },
+  {
+    title: "Object Oriented Programming with C++",
+    content: "Completed a course on NIIT with Outstanding performance.",
+    img_link: "/tiny_image/NIIT CPP.jpg",
+    document: "https://drive.google.com/file/d/1I8G99qiOvbz7Z7XIv_xAdLJWRkDwPzev/view?usp=sharing",
+  },
+  {
+    title: "Internal Hackathon for SIH 2020",
+    content: "First in college level Smart India Hackathon selection. Worked on Building an Android app for managing police records.",
+    img_link: "/tiny_image/Sih internal hackathon.jpg",
+    document: "https://drive.google.com/file/d/1HdRUnk9pncSvTGVKaG2mm3UFCzcA0Csm/view?usp=sharing",
+  },
+  {
+    title: "Infosys Hackathon 2020",
+    content: "Made to 6th out of 40 teams participated. Worked on Building an Android app for FOOD RE-DISTRIBUTION to Orphans and needy people from food sources like hotels or parties.",
+    img_link: "/tiny_image/Infosys Hackathon 2020.jpg",
+    document: "https://drive.google.com/file/d/1Ha67XfjDpe6pmR2pu_0XJtWKeGqclXpc/view?usp=sharing",
+  },
+  {
+    title: "Ozmenta 2020",
+    content: "Presented a paper on my project Accumulator later renamed as TOPNINE in Velammal Engineering College.",
+    img_link: "/tiny_image/Vellamal Paper Presentation.jpg",
+    document: "https://drive.google.com/file/d/1HeX5nywxvYjUP0hSypUo5zgEI_CKpim-/view?usp=sharing",
   },
 ];
-achieve_data.reverse();
+// achieve_data.reverse();
 // --------------------------------------------------------------------------
 achievecerti = document.getElementById("achievecerti");
 // achieveenter(3);
@@ -50,7 +73,7 @@ function achieveenter(limit) {
 										</div>
 									</div>
 									<div class="col-md-4 my-auto">
-										<a href="${value.document}">
+										<a href="${achieve_data[value].document}" target="_blank">
 										<button class="btn btn-outline-primary float-md-right text-nowrap mt-2 mb-2"
 											onMouseOver="this.style.color='white'"
 											onMouseOut="this.style.color='#4776E6'">
@@ -68,42 +91,77 @@ certi_data = [
     title: "Introduction to Python",
     content: "This is a Course completion certificate from datacamp",
     img_link: "/tiny_image/intro_python.jpg",
-    document: "/documents/introductiontopython.pdf",
+    document: "https://drive.google.com/file/d/1VdGOUlvnm7EKITxsbjlMuxWVvIzvgxA_/view?usp=sharing",
     type: "complete"
   },
   {
     title: "Introduction to SQL",
     content: "This is a Course completion certificate from datacamp",
     img_link: "/tiny_image/intro_sql.jpg",
-    document: "/documents/introductiontosql.pdf",
+    document: "https://drive.google.com/file/d/1DWpzjvNcoDOKFnY4dQIi3-Ax1k-wCdDS/view?usp=sharing",
     type: "complete"
   },
   {
     title: "Introduction to Databases in Python",
     content: "This is a Course completion certificate from datacamp",
     img_link: "tiny_image/intro_to_db_in_python.jpg",
-    document: "/documents/introtodbinpython.pdf",
+    document: "https://drive.google.com/file/d/1MtweoR91ZCbpkJZp0Lc6S5PSWI-MyOUH/view?usp=sharing",
     type: "complete"
   },
   {
     title: "Intermediate Python",
     content: "This is a Course completion certificate from datacamp",
     img_link: "tiny_image/intermediatepython.jpg",
-    document: "documents/intermediatepython.pdf",
+    document: "https://drive.google.com/file/d/185375Fc_6Sf2UGl7YYxHUaeH_fk3lrS3/view?usp=sharing",
     type: "complete"
   },
   {
     title: "Unsupervised Learning in Python",
     content: "This is a Course completion certificate from datacamp",
     img_link: "tiny_image/unsupervisedlearninginpython.jpg",
-    document: "documents/unsupervisedlearninginpython.pdf",
+    document: "https://drive.google.com/file/d/1gBfwagq6vml5GukqV6m1xxICfoTlU3c1/view?usp=sharing",
     type: "complete"
+  },
+  {
+    title: "Introduction to Deep Learning in Python",
+    content: "This is a Course completion certificate from datacamp",
+    img_link: "tiny_image/intro_to_deep.jpg",
+    document: "https://drive.google.com/file/d/1UFvUfMSrjwC2QNT8_zvr6JTMoPAPtVWI/view?usp=sharing",
+    type: "complete"
+  },
+  {
+    title: "Machine Learning for Time Series Data in Python",
+    content: "This is a Course completion certificate from datacamp",
+    img_link: "tiny_image/ml_for_time_series.jpg",
+    document: "https://drive.google.com/file/d/15arKm988tar8X3FkxPaFvUiqIp-45CsR/view?usp=sharing",
+    type: "complete"
+  },
+  {
+    title: "Preprocessing for Machine Learning in Python",
+    content: "This is a Course completion certificate from datacamp",
+    img_link: "tiny_image/preprocess_for_ml.jpg",
+    document: "https://drive.google.com/file/d/1ImC4plM_q-M9jJUO-Y_iITRvNqlobxis/view?usp=sharing",
+    type: "complete"
+  },
+  {
+    title: "Introduction to Predictive Analytics in Python",
+    content: "This is a Course completion certificate from datacamp",
+    img_link: "tiny_image/intro_to_predictive.jpg",
+    document: "https://drive.google.com/file/d/1-Uec39aaodU0UcvJLdso7pTGJQ2NYX_2/view?usp=sharing",
+    type: "complete"
+  },
+  {
+    title: "Programming, Data Structures and Algorithms Using Python",
+    content: "This is a Certification Course from NPTEL ( ELITE )",
+    img_link: "tiny_image/Programming, Data Structures and Algorithms Using Python.jpg",
+    document: "https://drive.google.com/file/d/1upSnxo6djf1LnS_T5KPcFgDRCvoGUzx2/view?usp=sharing",
+    type: "course"
   },
   {
     title: "Joy of Computing in Python",
     content: "This is a Certification Course from NPTEL ( ELITE - Silver )",
     img_link: "tiny_image/joyofcomputingwithpython.jpg",
-    document: "documents/Joyofcomputingwithpython.jpg",
+    document: "https://drive.google.com/file/d/1E1zFNmZF6n8QTNXXeyhj-Xl-dR_0gPkG/view?usp=sharing",
     type: "course"
   }
 ];
@@ -144,7 +202,7 @@ function certifilter(typee, limit, flag) {
 										</div>
 									</div>
 									<div class="col-md-4 my-auto">
-										<a href="${certi_data[value].document}">
+										<a href="${certi_data[value].document}" target="_blank">
 										<button class="btn btn-outline-primary float-md-right text-nowrap mt-2 mb-2"
 											onMouseOver="this.style.color='white'"
 											onMouseOut="this.style.color='#4776E6'">
